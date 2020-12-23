9380d3f8046ac2c801fed188201401e3c6f49b6c
cui.jiang <cui.jiang@tongdun.cn>
2020-12-23 11:52:09 +0800

    feat: shadowEqual

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './Click'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import App from './Click'
 
-ReactDOM.render(<App name="appComponent" />, document.getElementById('root'))
+// ReactDOM.render(<App name="appComponent" />, document.getElementById('root'))
+
+// 实际上是Object.is()的polyfill
+function is(x, y) {
+  // SameValue algorithm
+  debugger
+  if (x === y) {
+   // 处理为+0 != -0的情况
+    return x !== 0 || 1 / x === 1 / y;
+  } else {
+  // 处理 NaN === NaN的情况
+    return x !== x && y !== y;
+  }
+};
+
+console.log(is(null, null))
